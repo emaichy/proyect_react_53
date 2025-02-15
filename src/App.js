7faabed8aa44402c3cd876a0ce89d0c30b9975b8
emaichy <lalomonto16@gmail.com>
2025-02-15 08:19:24 -0600

    agrege algo xd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> yea perdonen kamehameha 
         </p>
         <a
           className="App-link"
